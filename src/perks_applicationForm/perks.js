80bc2066cba3ef84efc2c85653c6a03cd985960b
June-Azeneth <junesuco2001@gmail.com>
2023-10-14 16:31:52 +0800

    Initial Design for perks and application form section

---
+++ src/perks_applicationForm/perks.js
@@ -0,0 +1,60 @@
+//styles import
+import './styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+//assets import
+import Accesibility from './assets/images/accesibility.png';
+import Allowance from './assets/images/allowance.png';
+import Gatherings from './assets/images/gathering.png';
+import Mentorship from './assets/images/mentorship.png';
+import Picture from './assets/images/picBg.png'
+
+//components import
+import NavBar from './navbar';
+import Form from './application_form'
+
+function Perks() {
+    return (
+        <div>
+            <NavBar />
+            <div className='perksContainer'>
+                <h4 className='headerWeight t-lg'>INTERNSHIP <span className='t-lg color-main'>PERKS</span></h4>
+                <div className='cardContainer'>
+                    <div className='card'>
+                        <h5 className='cardTitle color-accent t-md t-bold'>MENTORSHIP</h5>
+                        <img className='image' src={Mentorship} alt="" />
+                        <p className='cardContent color-accent'>In mentoring, we don't just teach, we inspire growth.</p>
+                    </div>
+                    <div className='card'>
+                        <h5 className='cardTitle color-accent t-md t-bold'>ALLOWANCE</h5>
+                        <img className='image' src={Allowance} alt="" />
+                        <p className='cardContent color-accent'>In mentoring, we don't just teach, we inspire growth.</p>
+                    </div>
+                    <div className='card'>
+                        <h5 className='cardTitle color-accent t-md t-bold'>ACCESIBILITY</h5>
+                        <img className='image' src={Accesibility} alt="" />
+                        <p className='cardContent color-accent'>In mentoring, we don't just teach, we inspire growth.</p>
+                    </div>
+                    <div className='card'>
+                        <h5 className='cardTitle color-accent t-md t-bold'>GATHERINGS</h5>
+                        <img className='image' src={Gatherings} alt="" />
+                        <p className='cardContent color-accent'>In mentoring, we don't just teach, we inspire growth.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='background-pic'>
+                <Form className='absolute' />
+            </div>
+         
+
+
+
+
+        </div>
+    );
+}
+
+export default Perks;
