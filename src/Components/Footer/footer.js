9c45cde929c20d7ed138d9eb14d2a66a8fc8d62e
June-Azeneth <junesuco2001@gmail.com>
2023-10-22 12:04:05 +0800

    Fixed footer icons

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,7 +1,7 @@
 //styles import
 import '../../css/styles.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import discordSVG from "../../Assets/svg/discordsvg.svg"
 import emailSVG from "../../Assets/svg/emailsvg.svg"
@@ -10,34 +10,34 @@ import githubSVG from "../../Assets/svg/githubsvg.svg"
 
 import React from 'react';
 
-function Footer(){
+function Footer() {
     return (
         <div className="container-fluid bg-body-secondary">
-        <div className='container'>
-       <div className='grid-container d-flex justify-content-center'>
-       <div class="row">
-           <div class="col-3 d-flex justify-content-center">
-                <a href='#'><img src={discordSVG} className='svg-footer-icons'/></a>
-           </div>
-       <div class="col-3 d-flex justify-content-center">
-               <a href='#'><img src ={emailSVG} className='svg-footer-icons'/></a>
-           </div>
-       <div class="col-3 d-flex justify-content-center">
-               <a href='https://www.facebook.com/MYTSoftDevSolutions'><img src = {facebookSVG} className='svg-footer-icons'/></a>
-           </div>
-       <div class="col-3 d-flex justify-content-center">
-             <a href='#'><img src ={githubSVG} className='svg-footer-icons' /></a>
-           </div>
-       </div>
+            <div className='container pb-3'>
+                <div className='grid-container d-flex justify-content-center'>
+                    <div class="soc-med-icons d-flex gap-4">
+                        <div class="svg-footer-icons">
+                            <a href='#'><img src={discordSVG} className='svg-footer-icons' /></a>
+                        </div>
+                        <div class="svg-footer-icons">
+                            <a href='#'><img src={emailSVG} className='svg-footer-icons' /></a>
+                        </div>
+                        <div class="svg-footer-icons">
+                            <a href='https://www.facebook.com/MYTSoftDevSolutions'><img src={facebookSVG} className='svg-footer-icons' /></a>
+                        </div>
+                        <div class="svg-footer-icons">
+                            <a href='#'><img src={githubSVG} className='svg-footer-icons' /></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col d-flex justify-content-center mt-3">
+                    <h5 className="footer-font">MYT SoftDev Solutions Incoporated</h5>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <h6 className="footer-font">Innovative. Intutiative. Powerful.</h6>
+                </div>
+            </div>
         </div>
-        <div class = "col d-flex justify-content-center mt-3">
-               <h5 className ="footer-font">MYT SoftDev Solutions Incoporated</h5>
-           </div>
-           <div class ="col d-flex justify-content-center">
-                <h6 className ="footer-font">Innovative. Intutiative. Powerful.</h6>
-           </div>
-       </div>
-   </div>
     );
 }
 export default Footer;
